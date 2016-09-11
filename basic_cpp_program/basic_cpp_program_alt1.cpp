a04991845ca69a858429347dfb8d12d4f38db3e5
PaulConradRCC <pjc@pauljconrad.net>
2016-09-10 18:29:56 -0700

    Commit alternative methods for using namespaces.

---
+++ basic_cpp_program/basic_cpp_program_alt1.cpp
@@ -0,0 +1,13 @@
+// Basic C++ Program
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello, C++ students!!!"; // output the text (without a newline at the end): Hello, C++ students!!!
+	cout << endl; // output a newline after the last ! in the above output statement
+	
+	return 0; // return a "code" of 0 back to the operating system when our main function is done
+}
---
+++ basic_cpp_program/basic_cpp_program_alt2.cpp
@@ -0,0 +1,10 @@
+// Basic C++ Program
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, C++ students!!!"; // output the text (without a newline at the end): Hello, C++ students!!!
+	std::cout << std::endl; // output a newline after the last ! in the above output statement
+	
+	return 0; // return a "code" of 0 back to the operating system when our main function is done
+}

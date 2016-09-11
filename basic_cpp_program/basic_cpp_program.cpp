e5a1e4bf161d0a703eab01e679ff7016ea4f0f9e
PaulConradRCC <pjc@pauljconrad.net>
2016-09-10 17:27:49 -0700

    Initial commit for basic_cpp_program.cpp

---
+++ basic_cpp_program/basic_cpp_program.cpp
@@ -0,0 +1,12 @@
+// Basic C++ Program
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, C++ students!!!"; // output the text (without a newline at the end): Hello, C++ students!!!
+	cout << endl; // output a newline after the last ! in the above output statement
+	
+	return 0; // return a "code" of 0 back to the operating system when our main function is done
+}

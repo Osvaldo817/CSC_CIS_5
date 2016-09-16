c52ea7f61f0005a7d30c94b7871bc78de1831799
rcc <rcc>
2016-09-15 18:50:19 -0700

    Initial commit for Named_Constants

---
+++ Named_Constants/Named_Constants.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	// example named constants
+	const float PI = 3.14159;
+	const int MAXIMUM_SEATS = 42;
+	
+	const float TWO_PI = PI*2;
+	
+	// output the constants
+	std::cout << "PI=" << PI << std::endl;
+	std::cout << "MAXIMUM_SEATS=" << MAXIMUM_SEATS << std::endl;
+	std::cout << "PI times 2=" << TWO_PI << std::endl;
+	
+	// return from main function
+	return 0;
+}

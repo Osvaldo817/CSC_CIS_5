71e44d9cc1ffe46c097982e1854013f18854934d
rcc <rcc>
2016-11-17 19:21:30 -0800

    Enhanced the random_fill function to fill the array with values between minValue and maxValue.

--- Discussion111716/arrays_1117.cpp
+++ Discussion111716/arrays_1117.cpp
@@ -12,10 +12,10 @@ void output_array(int a[], int sz);
 
 int main()
 {
-	const int SIZE = 5;
+	const int SIZE = 25;
 	int my_array[SIZE];
 	
-	random_fill(my_array,SIZE);
+	random_fill(my_array,SIZE,-10,10);
 	output_array(my_array,SIZE);
 	
 	cout << "Sum of the array is: " << total(my_array,SIZE) << endl;
@@ -39,9 +39,13 @@ int total(int a[], int sz)
 	return sum;
 }
 
-void random_fill(int a[], int sz)
+void random_fill(int a[], int sz, int minValue, int maxValue)
 {
+	// added functionality to fill array with random values between
+	// minValue and maxValue inclusive
+	int range = (maxValue+1)-minValue;
+	
 	srand(time(0));
 	for(int i=0;i<sz;i++)
-		a[i]=rand();
+		a[i]=minValue+rand() % range;
 }

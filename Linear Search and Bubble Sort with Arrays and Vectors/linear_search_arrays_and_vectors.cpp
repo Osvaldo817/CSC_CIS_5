8acb98c84511d1d93403a79578c1f96d881237aa
rcc <rcc>
2017-05-11 19:30:36 -0700

    Update with sort function

--- Linear Search and Bubble Sort with Arrays and Vectors/linear_search_arrays_and_vectors.cpp
+++ Linear Search and Bubble Sort with Arrays and Vectors/linear_search_arrays_and_vectors.cpp
@@ -23,10 +23,53 @@ int linear_search_v(vector<int> v, int target)
 	return -1;
 }
 
+// sort functions
+void sort(int a[], int sz)
+{
+	bool swap;
+	
+	do 
+	{
+		swap=false;
+		for(int i=0;i<sz-1;i++)
+		{
+			if ( a[i] > a[i+1] )
+			{
+				int temp = a[i];
+				a[i] = a[i+1];
+				a[i+1] = temp;
+				swap=true;
+			}
+		}	
+	} while (swap);
+	
+}
+
+// overloaded vector version of sort
+void sort(vector<int> & v)
+{
+	bool swap;
+	
+	do 
+	{
+		swap=false;
+		for(int i=0;i<v.size()-1;i++)
+		{
+			if ( v[i] > v[i+1] )
+			{
+				int temp = v[i];
+				v[i] = v[i+1];
+				v[i+1] = temp;
+				swap=true;
+			}
+		}	
+	} while (swap);
+}
+
 // main function
 int main()
 {
-	const int SZ = 5;
+	const int SZ = 25;
 	
 	srand( time(0) );
 	
@@ -36,22 +79,32 @@ int main()
 	// fill array a with SZ random numbers
 	for(int i=0;i<SZ;i++)
 	{
-		a[i]=rand();
+		a[i]=rand() % 100;
 		v[i]=a[i];
 	}
-		
-	cout << "Random numbers in array: ";
-	for(int i=0;i<SZ-1;i++)
-		cout << a[i] << ", ";
 	
+	sort(a,SZ); // sort array 'a'
+	sort(v); // sort vector 'v'
+	
+	// output contents of array 'a'
+	cout << "Random numbers in array: " << endl;
+	for(int i=0;i<SZ-1;i++)
+	{
+		cout << a[i] << ", ";	
+		if ( (i+1)%5==0 ) cout << endl;
+	}
 	cout << a[SZ-1] << endl;
 	
-	cout << "Random numbers in vector: ";
+	// output contents of vector 'v'
+	cout << "Random numbers in vector: " << endl;
 	for(int i=0;i<v.size()-1;i++)
-		cout << v[i] << ", ";
-	
+	{
+		cout << v[i] << ", ";	
+		if ( (i+1)%5==0 ) cout << endl;
+	}
 	cout << v[v.size()-1] << endl;
 	
+	// demonstrate linear searching
 	int target;
 	cout << "Enter an integer for me to find: ";
 	cin >> target;
